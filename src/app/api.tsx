2ffcdef7804ae7efa05cc17bf11f9a25184b6f11
Sebastian Melendez <sebm@seas.upenn.edu>
2023-06-18 22:43:08 -0600

    pushing

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -3,7 +3,7 @@ require("dotenv").config();
 export async function getData(startBlock: number, endBlock: number): Promise<any> {
     const endpoint = process.env.NEXT_PUBLIC_ENDPOINT;
     console.log(endpoint);
-    return fetch(`${endpoint}/api/check?startBlock=${startBlock}&endBlock=${endBlock}`)
+    return fetch(`https://pudgy-pen-test.vercel.app/api/check?startBlock=${startBlock}&endBlock=${endBlock}`)
         .then((response) => {
             if (response.ok) {
                 return response.json();
